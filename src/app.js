52837ed5f7af48465cb4845ea8782fe6c703a326
francorom99 <161725675+francorom99@users.noreply.github.com>
2024-06-05 22:13:10 +0000

    ¨franco¨

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+  console.log("Hello ");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class 🥺",
+    "when I was sleeping 😴",
+    "while I was exercising 🏋️",
+    "during my lunch 🥪",
+    "while I was praying 🙏"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
